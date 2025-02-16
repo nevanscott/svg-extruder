a9365dcab03da4831b08c147dfd4b272f9fb1792
Nevan Scott <nfscott@gmail.com>
2025-02-16 02:14:27 +0100

    spoke a little too soon

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/**/*.{js}");
 
   // Pass through the JavaScript file (transformSvgToIsometric.js)
-  eleventyConfig.addPassthroughCopy("src/content/assets/transform.js");
+  eleventyConfig.addPassthroughCopy("website/src/content/assets/transform.js");
 
   // Specify directories for Eleventy to read from
   return {
